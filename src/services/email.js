47be319e690783aeb9681736a1cbd63e769e75f0
MinJunSeo <smj0385@naver.com>
2021-03-07 16:25:02 +0900

    feat: EmailService

---
+++ src/services/email.js
@@ -0,0 +1,34 @@
+const { sendEmail } = require('../configs/server');
+const transporter = require('../repositories');
+const makeUuid = require('../utils');
+
+class EmailService {
+  constructor() {
+    this.transporter = transporter;
+  }
+
+  async sendEmail(req) {
+    try {
+      const info = await transporter.sendMail({
+        from: `noreply ${sendEmail}`,
+        to: `${req.body.name} <${req.body.receiveEmail}>`,
+        subject: 'Please verify your device',
+        html: `
+        <p>Hi, ${req.body.name}!</p>
+        <p>A sing in attempt requires further verfication \
+        because we did not recognize your device.</p>
+        <p>Verfication code: ${makeUuid()}</p>
+        `
+      });
+    } catch (error) {
+      console.error(error.message);
+      console.error(error.stack);
+
+      error.status = 500;
+      error.message = 'Internal Server Error';
+      throw error;
+    }
+  }
+}
+
+module.exports = EmailService;
