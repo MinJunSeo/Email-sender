6a0b5288ccf4724baadd1910368fa59d89985bee
MinJunSeo <smj0385@naver.com>
2021-03-07 16:24:23 +0900

    feat: index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,6 @@
-const emailRouter = require('./email');
+const router = require('express').Router();
+const EmailRouter = require('./email');
+
+router.use('/email', EmailRouter);
+
+module.exports = router;
